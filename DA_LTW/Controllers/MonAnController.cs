57f15780a150a90f9137f8bd7fa372e6d25c9291
tms205 <manh27062005@gmail.com>
2025-11-13 11:20:24 +0700

    Up project

---
+++ DA_LTW/Controllers/MonAnController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DA_LTW.Controllers
+{
+    public class MonAnController : Controller
+    {
+        // GET: MonAn
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
